6a890c9a4ba431566a56871246fd68aa9c457fc8
Bharti Kuldeep <bhartikuldeep2001@rediffmail.com>
2026-03-14 11:13:27 +0530

    Add custom 404 error page

---
+++ resources/views/errors/404.php
@@ -0,0 +1,10 @@
+<section class="page-shell">
+    <div class="container form-shell">
+        <div class="form-card">
+            <span class="kicker">404</span>
+            <h1 class="page-title">Page Not Found</h1>
+            <p class="page-subtitle">That route does not exist. Try heading back home.</p>
+            <a href="/" class="button">Go home</a>
+        </div>
+    </div>
+</section>
